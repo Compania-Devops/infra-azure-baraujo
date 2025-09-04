6a7c786ca019ab107be7fad80f65ec97433b7b5d
Brandon Araujo <bac2795@gmail.com>
2025-09-03 19:21:40 -0500

    Fix

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-cicd-terraform-app-baraujo" # Reemplazar por baraujo
-    storage_account_name = "tfstatebaraujo"                # Reemplazar por baraujo
+    resource_group_name  = "rg-cicd-terraform-app-baraujo" # Reemplazar por apellido
+    storage_account_name = "tfstatebaraujo"                # Reemplazar por apellido
     container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
   }
